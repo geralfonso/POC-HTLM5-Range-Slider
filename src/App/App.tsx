0338abdb8f4c616bc18d642d81fb8ef9e3d78708
German Alfonso <german.alfonso@endava.com>
2019-12-09 10:49:14 -0500

    Input range complete

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,23 +3,34 @@ import * as React from "react";
 import "./App.scss";
 import RangeSlider from "./RangeSlider";
 const App: React.FC = (): React.ReactElement => {
+  function handleOnChange(min: number, max: number): void {
+    console.log(`Min: ${min}, Max: ${max}`);
+  }
   return (
     <div className="App">
       <div>
         <h3>Range Slider without steps and no default values</h3>
         <div>
-          <RangeSlider min={30} max={130} step={0} />
+          <RangeSlider
+            label="This is a label"
+            max={50}
+            min={0}
+            onChange={handleOnChange}
+            step={10}
+          />
         </div>
       </div>
       <div>
-        <h3>Range Slider witho steps and default values</h3>
+        <h3>Range Slider with steps and default values</h3>
         <div>
           <RangeSlider
-            min={20}
-            max={100}
-            step={10}
-            defaultMin={50}
-            defaultMax={80}
+            isOptional
+            label="Basic Range with text bottom"
+            max={1000}
+            maxValue={700}
+            min={100}
+            minValue={400}
+            step={100}
           />
         </div>
       </div>
--- src/App/RangeSlider/RangeSlider.tsx
+++ src/App/RangeSlider/RangeSlider.tsx
@@ -1,151 +1,238 @@
 import * as React from "react";
+import cx from "classnames";
 
 import "./RangeSlider.scss";
 
-const CURRENTMIN: string = "currentMin";
-const CURRENTMAX: string = "currentMax";
+const CURRENT_MIN: string = "currentMin";
+const CURRENT_MAX: string = "currentMax";
+const RANGE_SLIDER_MAX_WIDTH: number = 300;
+const HALF_THUMB_SIZE: number = 8;
 
+type onDragArrow = (event: React.ChangeEvent<HTMLInputElement>) => void;
+
+enum RangeSliderDirection {
+  Horizontal = "horizontal",
+  Vertical = "vertical"
+}
+
+interface CustomCssVars extends React.CSSProperties {
+  "--start"?: string;
+  "--end"?: string;
+  "--pos"?: number;
+  "--dist"?: string;
+  "--left"?: string;
+}
 interface ITooltip extends React.CSSProperties {
   "--tooltipPos": string;
 }
 
 interface IRangeSlider {
-  min: number;
   max: number;
-  step: number;
-  defaultMin?: number;
-  defaultMax?: number;
+  min: number;
+  label: string;
+  step?: number;
+  minValue?: number;
+  maxValue?: number;
+  width?: number;
+  className?: string;
+  isOptional?: boolean;
+  showTextInput?: boolean;
+  direction?: RangeSliderDirection;
+  dataUI?: string;
+  onChange?(minValue: number, maxValue: number): void;
 }
 
 const RangeSlider: React.FC<IRangeSlider> = ({
   min,
   max,
   step,
-  defaultMin,
-  defaultMax
+  minValue,
+  maxValue,
+  width,
+  label,
+  className,
+  isOptional,
+  direction,
+  dataUI,
+  onChange
 }: IRangeSlider): React.ReactElement => {
-  const [currentMin, setCurrentMin] = React.useState<number>(min);
-  const [currentMax, setCurrentMax] = React.useState<number>(max);
+  const [currentMin, setCurrentMin] = React.useState<number>(minValue || min);
+  const [currentMax, setCurrentMax] = React.useState<number>(maxValue || max);
 
-  React.useEffect(() => {
-    defaultMin !== undefined && setCurrentMin(defaultMin);
-    defaultMax !== undefined && setCurrentMax(defaultMax);
-  }, []);
+  React.useEffect((): void => {
+    const updateValues = (): void => {
+      if (onChange) {
+        onChange(currentMin, currentMax);
+      }
+    };
+    updateValues();
+  }, [currentMin, currentMax]);
 
-  function onDrag(event: React.ChangeEvent<HTMLInputElement>): void {
+  const onDrag: onDragArrow = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
     event.persist();
+
     const {
       target: { name, value }
     } = event;
 
-    if (name === CURRENTMIN && Number(value) <= currentMax) {
+    if (name === CURRENT_MIN && Number(value) <= currentMax) {
       setCurrentMin(Number(value));
     }
-    if (name === CURRENTMAX && Number(value) >= currentMin) {
+    if (name === CURRENT_MAX && Number(value) >= currentMin) {
       setCurrentMax(Number(value));
     }
-  }
+  };
 
   function getPercent(initial: number, end: number): number {
     return Math.round((initial / end) * 100);
   }
 
-  function getProgressStyle(): React.CSSProperties {
-    const progressStart = getPercent(currentMin - min, max - min);
-    const progressEnd = getPercent(currentMax - min, max - min);
+  const getProgressBar: () => CustomCssVars = (): CustomCssVars => {
+    const start: number = getPercent(currentMin - min, max - min) - 0.2;
+    const end: number = getPercent(currentMax - min, max - min) + 0.2;
 
     return {
-      left: `${progressStart}%`,
-      width: `${progressEnd - progressStart}%`
+      "--start": `${start}%`,
+      "--end": `${end}%`
     };
-  }
+  };
 
-  function* range(start: number, end: number): Iterable<number> {
-    if (step <= 0) {
-      yield min;
-      yield max;
-      return;
-    }
-    if (min + start * step > max) return;
-    yield start;
-    if (start >= end) return;
-    yield* range(start + 1, end);
-  }
-
-  function displayValues(index: number): number {
-    if (step <= 0) {
-      return index ? max : min;
-    }
-    return min + index * step;
-  }
-
-  function renderStepsOrValues(renderValues?: boolean): React.ReactElement[] {
-    const rangeProgress: number = max - min;
-    const howManey: number = step ? rangeProgress / step : 1;
-    return Array.from(range(0, howManey), (_: number, index: number) => {
-      return (
-        <span
-          key={index}
-          style={{
-            left: `${(100 / howManey) * index}%`
-          }}
-        >
-          {renderValues && displayValues(index)}
-        </span>
-      );
-    });
-  }
-
-  function getTooltipPos(currentPos: number): ITooltip {
+  const getTooltipPosition: (currentPos: number) => CustomCssVars = (
+    currentPos: number
+  ): CustomCssVars => {
     return {
-      "--tooltipPos": `${Math.round(100 * ((currentPos - min) / (max - min)))}%`
+      "--pos": (currentPos - min) / (max - min),
+      "--dist": `${width}px`
     };
+  };
+
+  function range(start: number, end: number, stepIncrement: number): number[] {
+    if (!stepIncrement) return [start, end];
+    if (start === end) return [start];
+    if (start > end) return [];
+    return [start, ...range(start + stepIncrement, end, stepIncrement)];
   }
+
+  function getStepLeftPosition(
+    stepValue: number,
+    minAllowed: number,
+    maxAllowed: number
+  ): number {
+    return (
+      ((maxAllowed - minAllowed) * (stepValue - min)) / (max - min) + minAllowed
+    );
+  }
+
+  function renderStepValues(): React.ReactNode[] {
+    return range(min, max, Number(step)).map(
+      (stepValue: number, index: number): React.ReactNode => {
+        // To get the marks exactly in middle of the each thumb
+        const left: number = getStepLeftPosition(
+          stepValue,
+          HALF_THUMB_SIZE,
+          Number(width) - HALF_THUMB_SIZE
+        );
+
+        return (
+          <span
+            className="va-range-slider-step-marks"
+            key={index}
+            style={{ left: `${left.toFixed(2)}px` }}
+          >
+            {stepValue}
+          </span>
+        );
+      }
+    );
+  }
+
+  const classList: string = cx("va-range-slider", className);
+
+  const containerClassList: string = cx("va-range-slider-container", {
+    "va-range-slider-container--vertical":
+      direction === RangeSliderDirection.Vertical,
+    "va-range-slider-container--horizontal":
+      direction === RangeSliderDirection.Horizontal
+  });
 
   console.log(`currentMin: ${currentMin}, currentMax: ${currentMax}`);
   return (
-    <div className="va-range-slider">
-      <div className="va-range-slider-container">
-        <div
-          className="va-range-slider--slider-bar"
-          style={getProgressStyle()}
-        />
-        <div className="va-range-slider--step">{renderStepsOrValues()}</div>
-        <input
-          className="va-range-slider--input va-range-slider--min"
-          type="range"
-          name={CURRENTMIN}
-          value={currentMin}
-          onChange={onDrag}
-          step={step}
-          min={min}
-          max={max}
-          data-tooltip={currentMin}
-          style={getTooltipPos(currentMin)}
-        />
-
-        <input
-          className="va-range-slider--input va-range-slider--max"
-          type="range"
-          name={CURRENTMAX}
-          value={currentMax}
-          onChange={onDrag}
-          step={step}
-          min={min}
-          max={max}
-          data-tooltip={currentMax}
-          style={getTooltipPos(currentMax)}
-        />
+    <div className={classList}>
+      {label && (
+        <label className="va-form-label" data-ui={`${dataUI}_label`}>
+          {label}
+          {isOptional && <div className="u-label--optional">Optional</div>}
+        </label>
+      )}
+      <div className={containerClassList}>
+        <div className="va-range-slider-wrapper">
+          <div className="va-range-slider-track-wrapper" style={{ width }}>
+            <div
+              className="va-range-slider-progress-bar"
+              style={getProgressBar()}
+            />
+            <input
+              aria-valuemax={max}
+              aria-valuemin={min}
+              aria-valuenow={currentMin}
+              className="va-range-slider-input va-range-slider-thumb--min"
+              data-ui={`${dataUI}_thumb--min`}
+              max={max}
+              min={min}
+              name={CURRENT_MIN}
+              onChange={onDrag}
+              role="slider"
+              type="range"
+              value={currentMin}
+            />
+            <output
+              className="va-range-slider-tooltip"
+              htmlFor={CURRENT_MIN}
+              style={getTooltipPosition(currentMin)}
+            >
+              {currentMin}
+            </output>
+            <input
+              aria-valuemax={max}
+              aria-valuemin={min}
+              aria-valuenow={currentMax}
+              className="va-range-slider-input va-range-slider-thumb--max"
+              ddata-ui={`${dataUI}_thumb--max`}
+              max={max}
+              min={min}
+              name={CURRENT_MAX}
+              onChange={onDrag}
+              onFocus={onDrag}
+              role="slider"
+              type="range"
+              value={currentMax}
+            />
+            <output
+              className="va-range-slider-tooltip"
+              htmlFor={CURRENT_MAX}
+              style={getTooltipPosition(currentMax)}
+            >
+              {currentMax}
+            </output>
+          </div>
+          <div className="va-range-slider-values">{renderStepValues()}</div>
+        </div>
       </div>
-      <div className="va-range-slider-values">{renderStepsOrValues(true)}</div>
     </div>
   );
 };
 
+RangeSlider.displayName = "RangeSlider";
+
 RangeSlider.defaultProps = {
-  min: 10,
-  max: 100,
-  step: 0
+  step: 0,
+  width: RANGE_SLIDER_MAX_WIDTH,
+  isOptional: false,
+  showTextInput: true,
+  direction: RangeSliderDirection.Horizontal,
+  dataUI: "range-slider"
 };
 
 export default RangeSlider;
